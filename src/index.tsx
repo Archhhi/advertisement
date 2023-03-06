4d7e55691c48459fe42ad0b4dc0e6ca438554762
Archhhi <brabus-20@mail.ru>
2023-03-06 09:46:34 +0300

    finished application

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'normalize.css'
+import 'normalize.css/normalize.css'
 import App from './App'
 
 import './assets/styles/global.css'
